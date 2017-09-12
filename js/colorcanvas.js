c69ae2d70503bc4c6c2e8808d88df69e2ddd43c6
Wolverine1621 <Wolverine1621@users.noreply.github.com>
2017-09-12 15:24:30 -0400

    Added drag/draw functionality

--- js/colorcanvas.js
+++ js/colorcanvas.js
@@ -1,21 +1,36 @@
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 
+var defaultColor;
+var defaultRadius;
+var globalMouseState = 0 // 0 is up, 1 is down
+var mouseJustUp = 0 // Handles the circle drawn immediately after the mouseup event
+
 c.width = window.innerWidth;
 c.height = window.innerHeight;
 
+// Main function
 function draw(e) {
   var clickX = e.pageX;
   var clickY = e.pageY;
-  var radius = generateRadius();
 
-  //ctx.fillStyle = "#2980b9";
-  ctx.fillStyle = generateRGB();
-  ctx.beginPath();
-  ctx.arc(clickX, clickY, radius, 0, 2 * Math.PI);
-  ctx.fill();
+  if (globalMouseState === 1 || mouseJustUp === 1) { // If circles are being drawn while mouse is down
+    ctx.fillStyle = defaultColor;
+    ctx.beginPath();
+    ctx.arc(clickX, clickY, defaultRadius, 0, 2 * Math.PI);
+    ctx.fill();
+    mouseJustUp = 0;
+  } else {
+    var radius = generateRadius();
+    ctx.fillStyle = generateRGB();
+    ctx.beginPath();
+    ctx.arc(clickX, clickY, radius, 0, 2 * Math.PI);
+    ctx.fill();
+  }
 }
 
+
+// Helper methods
 function generateRadius() {
   return Math.random() * (50 - 10) + 10;
 }
@@ -40,6 +55,8 @@ window.onresize = function() {
   c.height = window.innerHeight;
 }
 
+
+// Event handlers
 $(window).keypress(function(e) {
   if (e.keyCode == 0 || e.keyCode == 32) {
     document.getElementById("canvas").style.background = generateRGB();
@@ -50,4 +67,22 @@ $(document).keydown(function(e) {
   if (e.keyCode == 8) {
     ctx.clearRect(0, 0, c.width, c.height);
   }
+});
+
+// Drag and Draw functionality
+$(document).mousedown(function(){
+    globalMouseState = 1;
+    defaultColor = generateRGB();
+    defaultRadius = generateRadius();
+});
+
+$(document).mouseup(function(){
+    globalMouseState = 0;
+    mouseJustUp = 1;
+});
+
+$(document).mousemove(function(e){
+    if (globalMouseState === 1) {
+      draw(e);
+    }
 });
